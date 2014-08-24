ea348eb21f0eb6f2c73a8cd29a435d0c3219555e
Rafał Cymerys <rafal.cym@gmail.com>
2014-08-24 18:42:08 +0200

    Finished explosion

---
+++ Assets/HelicopterCollisionExplosion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterCollisionExplosion : MonoBehaviour {
+
+	public ParticleSystem Explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		Explosion.transform.position = this.transform.position;
+		Explosion.Play();
+		Destroy (this.gameObject);
+	}
+}
