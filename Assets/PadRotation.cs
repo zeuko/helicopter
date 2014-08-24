13e00798dc22459eec8e624d8868dd614d9fc131
Rafał Cymerys <rafal.cym@gmail.com>
2014-08-24 12:59:24 +0200

    Initial commit

---
+++ Assets/PadRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PadRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationSpeed = 1500.0f;
+		this.transform.Rotate (0.0f, 0.0f, rotationSpeed * Time.deltaTime);
+	}
+}
